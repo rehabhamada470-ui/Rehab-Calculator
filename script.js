6b4b04272cd4162ee38821a946450eeb99ff0a6f
rehabhamada470-ui <rehabhamada470@gmail.com>
2025-12-06 18:55:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function press(value) {
+    let display = document.getElementById("display");
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    let display = document.getElementById("display");
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "خطأ";
+    }
+}
